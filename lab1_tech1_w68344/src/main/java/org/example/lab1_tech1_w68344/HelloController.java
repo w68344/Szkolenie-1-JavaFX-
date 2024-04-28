8ac10ab5c250cda8d4404299686a4e5f652b2c04
Nikita <127506511+w68344@users.noreply.github.com>
2024-04-28 15:57:32 +0200

    lab1
    lab1

---
+++ lab1_tech1_w68344/src/main/java/org/example/lab1_tech1_w68344/HelloController.java
@@ -0,0 +1,14 @@
+package org.example.lab1_tech1_w68344;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
